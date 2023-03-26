bf4e7c2bb7f42b6d315c252d5b677fc8dfde6fab
tariq-almalki <e.tariq.almalki@gmail.com>
2023-03-26 11:14:02 +0300

    https for localhost with vite-plugin-mckert

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
+import { VitePWA } from 'vite-plugin-pwa';
+import react from '@vitejs/plugin-react';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		viteCompression({
+			algorithm: 'brotliCompress',
+		}),
+		VitePWA({ registerType: 'autoUpdate' }),
+		mkcert(),
+	],
+	server: {
+		https: true,
+	},
+});
